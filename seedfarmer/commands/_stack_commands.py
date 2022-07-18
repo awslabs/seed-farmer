176483850c9681619c1c52c961ab36f7a8a0076f
Chauncy McCaughey <chamcca@amazon.com>
2022-07-17 18:28:33 -0600

    isort

--- seedfarmer/commands/_stack_commands.py
+++ seedfarmer/commands/_stack_commands.py
@@ -12,11 +12,11 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from asyncio.log import logger
 import json
 import logging
 import os
 import time
+from asyncio.log import logger
 from typing import List, Optional
 
 from aws_codeseeder import codeseeder, commands, services

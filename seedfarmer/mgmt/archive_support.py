16d6f19f322857865387efa4e31589d572cd36c1
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-08-01 08:47:04 -0500

    add boto3 stubs

--- seedfarmer/mgmt/archive_support.py
+++ seedfarmer/mgmt/archive_support.py
@@ -12,15 +12,18 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import json
 import logging
 import os.path
 import pathlib
 import tarfile
-from typing import Tuple
-from urllib.parse import parse_qs, urlparse
+from typing import Optional
+from urllib.parse import urlparse
 from zipfile import ZipFile
 
+import boto3
 import requests
+from requests.auth import HTTPBasicAuth
 from requests.models import Response
 
 from seedfarmer import config
@@ -30,7 +33,13 @@ _logger: logging.Logger = logging.getLogger(__name__)
 parent_dir = os.path.join(config.OPS_ROOT, "seedfarmer.archive")
 
 
-def _download_archive(archive_url: str) -> Response:
+def _download_archive(archive_url: str, secret_arn: Optional[str]) -> Response:
+    if secret_arn:
+        region = "us-west-2"  # TODO: get toolchain region
+        sm_client = boto3.client("secretsmanager", region)
+
+        credentials = json.loads(sm_client.get_secret_value(SecretId=secret_arn)["SecretString"])
+
     # TODO: add a check here for an S3 HTTPS DNS, and if so, add the SigV4 Auth to the url
 
     if "s3.amazonaws" in archive_url:
@@ -42,7 +51,12 @@ def _download_archive(archive_url: str) -> Response:
         # active_url = create_signed_request(endpoint=active_url, session=session, credentials=credentials)
         # z = requests.get(active_url.url, headers=active_url.headers,allow_redirects=True)
     else:
-        resp = requests.get(archive_url, allow_redirects=True)
+        if credentials:
+            auth = HTTPBasicAuth(credentials["user"], credentials["password"])
+        else:
+            auth = None
+
+        resp = requests.get(archive_url, auth=auth, allow_redirects=True)
 
     return resp
 
@@ -74,27 +88,22 @@ def _process_archive(archive_name: str, response: Response, extracted_dir: str) 
     return os.path.join(parent_dir, extracted_dir)
 
 
-def _get_release_with_link(archive_url: str) -> Tuple[str, str]:
+def _get_release_with_link(archive_url: str, secret_arn: Optional[str]) -> str:
     parsed_url = urlparse(archive_url)
 
     if not parsed_url.scheme == "https":
         raise InvalidConfigurationError("This url must be via https: %s", archive_url)
 
-    query_params = parse_qs(parsed_url.query)
-    if not query_params.get("module"):
-        raise InvalidConfigurationError("module query param required : %s", archive_url)
-
-    module = query_params["module"][0]
     archive_name = parsed_url.path.replace("/", "_")
     extracted_dir = parsed_url.path.replace(".tar.gz", "").replace(".zip", "").replace("/", "_")
 
     if os.path.isdir(os.path.join(parent_dir, extracted_dir)):
-        return os.path.join(parent_dir, extracted_dir), module
+        return os.path.join(parent_dir, extracted_dir)
     else:
-        resp = _download_archive(parsed_url._replace(fragment="").geturl())
+        resp = _download_archive(parsed_url._replace(fragment="").geturl(), secret_arn)
 
         if resp.status_code == 200:
-            return _process_archive(archive_name, resp, extracted_dir), module
+            return _process_archive(archive_name, resp, extracted_dir)
 
         elif resp.status_code in [400, 403, 401, 302, 404]:
             _logger.error(f"Cannot find that archive at {archive_url}")
@@ -107,7 +116,7 @@ def _get_release_with_link(archive_url: str) -> Tuple[str, str]:
             )
 
 
-def fetch_module_repo(release_path: str) -> Tuple[str, str]:
+def fetch_module_repo(release_path: str, secret_arn: Optional[str] = None) -> str:
     """
     fetch_module_repo _summary_
 
@@ -117,12 +126,9 @@ def fetch_module_repo(release_path: str) -> Tuple[str, str]:
         The path passed in to fetch. If using a ProServe provided repo, this should look like
         archive::https://github.com/awslabs/idf-modules/archive/refs/tags/v1.10.0.zip?module=modules/dummy/dummy
 
-
     Returns
     -------
-    Tuple[str, str]
-        Returns a tuple that contains (in order):
-        - the full path of the seedfarmer.archive where the repo was cloned to
-        - the relative path under seedfarmer.archive of the module code
+    str:
+        the full path of the seedfarmer.archive where the repo was cloned to
     """
-    return _get_release_with_link(release_path.replace("archive::", ""))
+    return _get_release_with_link(release_path.replace("archive::", ""), secret_arn)

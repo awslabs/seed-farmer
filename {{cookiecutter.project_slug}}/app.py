1d6464c8b23abf3f512eae679b84a782ac2a52a5
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2024-08-21 11:55:19 -0500

    Update init module template (#705)

--- {{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/app.py
@@ -1,63 +1,37 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import os
-from typing import cast
+import aws_cdk
+import cdk_nag
 
-from aws_cdk import App, CfnOutput, Environment
-
+from settings import ApplicationSettings
 from stack import TemplateStack
 
-# Project specific
-project_name = os.getenv("SEEDFARMER_PROJECT_NAME")
-deployment_name = os.getenv("SEEDFARMER_DEPLOYMENT_NAME")
-module_name = os.getenv("SEEDFARMER_MODULE_NAME")
-
-if len(f"{project_name}-{deployment_name}") > 36:
-    raise ValueError("This module cannot support a project+deployment name character length greater than 35")
-
-
-def _param(name: str) -> str:
-    return f"SEEDFARMER_PARAMETER_{name}"
-
-
-def generate_description() -> str:
-    soln_id = os.getenv("SEEDFARMER_PARAMETER_SOLUTION_ID", None)
-    soln_name = os.getenv("SEEDFARMER_PARAMETER_SOLUTION_NAME", None)
-    soln_version = os.getenv("SEEDFARMER_PARAMETER_SOLUTION_VERSION", None)
-
-    desc = "My Module Default Description"
-    if soln_id and soln_name and soln_version:
-        desc = f"({soln_id}) {soln_name}. Version {soln_version}"
-    elif soln_id and soln_name:
-        desc = f"({soln_id}) {soln_name}"
-    return desc
-
-
-app = App()
+app_settings = ApplicationSettings()
+app = aws_cdk.App()
 
 template_stack = TemplateStack(
     scope=app,
-    id=f"{project_name}-{deployment_name}-{module_name}",
-    project_name=cast(str, project_name),
-    deployment_name=cast(str, deployment_name),
-    module_name=cast(str, module_name),
-    stack_description=generate_description(),
-    env=Environment(
-        account=os.environ["CDK_DEFAULT_ACCOUNT"],
-        region=os.environ["CDK_DEFAULT_REGION"],
+    id=app_settings.settings.app_prefix,
+    description=app_settings.parameters.module_description,
+    env=aws_cdk.Environment(
+        account=app_settings.default.account,
+        region=app_settings.default.region,
     ),
 )
 
-
-CfnOutput(
+aws_cdk.CfnOutput(
     scope=template_stack,
     id="metadata",
     value=template_stack.to_json_string(
         {
-            "TemplateOuptut1": "Add something from template_stack",
+            "TemplateOutput1": "Add something from template_stack",
         }
     ),
 )
+
+aws_cdk.Aspects.of(app).add(cdk_nag.AwsSolutionsChecks(log_ignores=True))
+
+aws_cdk.Tags.of(app).add("Deployment", app_settings.settings.app_prefix)
 
 app.synth()
--- {{cookiecutter.project_slug}}/tests/test_stack.py
+++ {{cookiecutter.project_slug}}/tests/test_stack.py
@@ -3,22 +3,54 @@
 
 import os
 import sys
+from unittest import mock
 
-import aws_cdk as cdk
+import aws_cdk
+import cdk_nag
 import pytest
-from aws_cdk.assertions import Template
+from aws_cdk.assertions import Annotations, Match, Template
 
 
 @pytest.fixture(scope="function")
 def stack_defaults():
-    os.environ["CDK_DEFAULT_ACCOUNT"] = "111111111111"
-    os.environ["CDK_DEFAULT_REGION"] = "us-east-1"
+    with mock.patch.dict(os.environ, {}, clear=True):
+        os.environ["CDK_DEFAULT_ACCOUNT"] = "111111111111"
+        os.environ["CDK_DEFAULT_REGION"] = "us-east-1"
 
-    # Unload the app import so that subsequent tests don't reuse
+        # Unload the app import so that subsequent tests don't reuse
+        if "stack" in sys.modules:
+            del sys.modules["stack"]
 
-    if "stack" in sys.modules:
-        del sys.modules["stack"]
-        
-        
-def test_app(stack_defaults):   
-    pass
+        yield
+
+
+@pytest.fixture(scope="function")
+def stack(stack_defaults) -> aws_cdk.Stack:
+    import stack
+
+    app = aws_cdk.App()
+
+    project_name = "test-project"
+    dep_name = "test-deployment"
+    mod_name = "test-module"
+    app_prefix = f"{project_name}-{dep_name}-{mod_name}"
+
+    return stack.TemplateStack(
+        app,
+        app_prefix,
+        # TODO: add props
+    )
+
+
+def test_synthesize_stack(stack: aws_cdk.Stack) -> None:
+    Template.from_stack(stack)
+
+
+def test_no_cdk_nag_errors(stack: aws_cdk.Stack) -> None:
+    aws_cdk.Aspects.of(stack).add(cdk_nag.AwsSolutionsChecks())
+
+    nag_errors = Annotations.from_stack(stack).find_error(
+        "*",
+        Match.string_like_regexp(r"AwsSolutions-.*"),
+    )
+    assert not nag_errors, f"Found {len(nag_errors)} CDK nag errors"

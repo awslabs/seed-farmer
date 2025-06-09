e79b76a036e68b983667ee345477045ac86a4001
kukushking <kukushkin.anton@gmail.com>
2025-06-09 17:52:41 +0100

    formatting

--- seedfarmer/commands/_parameter_commands.py
+++ seedfarmer/commands/_parameter_commands.py
@@ -212,7 +212,7 @@ def _module_metatdata(
         _logger.debug("loaded parameter value: %s", parameter_value)
 
         parameter_value = (
-            parameter_value.get(parameter.value_from.module_metadata.key, None) # type: ignore[assignment]
+            parameter_value.get(parameter.value_from.module_metadata.key, None)  # type: ignore[assignment]
             if parameter_value is not None and parameter.value_from.module_metadata.key is not None
             else parameter_value
         )
